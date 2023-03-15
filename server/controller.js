65eea5f8529e73967c84387bd4b1c0a31e65d11c
LucidRavings <cybernerd82@yahoo.com>
2023-03-15 17:46:50 -0600

    End of class time

--- server/controller.js
+++ server/controller.js
@@ -13,7 +13,6 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
   })
 
 module.exports = {
-
     getPendingAppointments: (req, res) => {
         sequelize.query(`
         SELECT *
@@ -30,6 +29,19 @@ module.exports = {
         FROM cc_users
         JOIN cc_clients
         ON cc_users.user_id = cc_clients.user_id
+        `).then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+
+    getPastAppointments: (req, res) => {
+        sequelize.query(`
+        SELECT a.appt_id, a.date, a.service_type, a.notes, u.first_name, u.last_name
+        FROM cc_appointments AS a
+        JOIN cc_emp_appts ea ON a.appt_id = ea.appt_id
+        JOIN cc_employees e ON e.emp_id = ea.emp_id
+        JOIN cc_users u ON e.user_id = u.user_id
+        WHERE a.approved = TRUE AND a.completed = TRUE
+        ORDER BY a.date DESC;
         `).then(dbRes => res.status(200).send(dbRes[0]))
         .catch(err => console.log(err))
     },
